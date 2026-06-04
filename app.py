601cf2bc7a69e9fcaa5943f2d21dde08adf66ec6
dianajulissa <85780914+dianajulissa@users.noreply.github.com>
2026-06-03 19:47:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+### Clase del lunes 01/06
+
 import streamlit as st
 
-# Ingreso de Título
+# Ingreso de Título en Cuerpo
 st.title("Proyecto Final Diploma Business Analyst")
 
 # Título en Barra lateral
@@ -15,3 +17,7 @@ st.image("logo_python.png", width = 500)
 st.sidebar.image("logo_dmc.png", width = 100)
 
 st.write("Elaborado por: Diana Córdova")
+
+### Clase del miércoles 03/06
+
+archivo = st.file_uploader("Cargue el archivo Excel o CSV")
