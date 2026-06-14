bd8ee3aa06b9d1b5dbf9fef668e3266451e358a8
dianajulissa <85780914+dianajulissa@users.noreply.github.com>
2026-06-13 20:48:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+###########################################################################################################################################
+
 # Librería Streamlit para Crear la Aplicación Web
 import streamlit as st
 
 # Librería Pandas para Leer Archivos CSV y Excel
 import pandas as pd
+
+###########################################################################################################################################
 
 # Título Principal de la Aplicación
 st.title("Diploma Business Analyst")
@@ -23,55 +27,71 @@ st.write("Elaborado por: Diana Córdova")
 # Despliega un Menú de Opciones en la Barra Lateral
 modulos = st.sidebar.selectbox("Seleccione un Módulo", ["Home", "Carga y Perfil del Dataset", "Procesamiento de Datos", "Análisis Visual"])
 
+###########################################################################################################################################
+
 if modulos == "Home":
 
     st.write("Bienvenido a la Aplicación")
+    st.write("Elaborado por: Diana Córdova")
 
     #if st.session_state.data is not None:
     #    st.success(f"Dataset cargado: {st.session_state.nombre_archivo}")
     #else:
     #    st.info("Aún no se ha cargado ningún dataset.")
 
+###########################################################################################################################################
+
 elif modulos == "Carga y Perfil del Dataset":
-  st.write("Carga y Perfil del Dataset")
-  
+    
+    st.write("Carga y Perfil del Dataset")
+    
+    # Permite la carga de un archivo por parte del usuario
+    archivo = st.file_uploader("Cargue el archivo Excel o CSV")
+
+    # Valida la Carga de un Archivo
+    if archivo is not None :
+
+        # Verificando que el archivo cargado tiene extensión .csv
+        if archivo.name.endswith(".csv"):
+            
+            # Lee el archivo CSV y lo guarda en un DataFrame
+            data = pd.read_csv(archivo)
+            
+            # Muestra el DataFrame en la aplicación
+            st.write(data)
+            
+        # Verificando que el archivo cargado tiene extensión .xlsx
+        elif archivo.name.endswith(".xlsx"):
+            
+            # Lee el archivo Excel y lo guarda en un DataFrame
+            data = pd.read_excel(archivo)
+            
+            # Muestra el DataFrame en la aplicación
+            st.write(data)
+            
+        # Si el archivo no es CSV ni Excel, mostramos un mensaje de error
+        else:
+            
+            st.write("Formato no válido")
+        
+    # Si el usuario no ha cargado ningún archivo, mostramos un mensaje
+    else :
+
+        st.write("Por favor cargue su archivo")
+
+###########################################################################################################################################
+
 elif modulos == "Procesamiento de Datos":
   st.write("Procesamiento de Datos")
-  
+
+###########################################################################################################################################
+
 elif modulos == "Análisis Visual":
   st.write("Análisis Visual")
 
+###########################################################################################################################################
 
 
-# Permite la carga de un archivo por parte del usuario
-archivo = st.file_uploader("Cargue el archivo Excel o CSV")
 
-# Valida si el usuario carga un archivo
-if archivo is not None :
 
-  # Valida si el archivo cargado tiene extensión .csv
-  if archivo.name.endswith(".csv"):
-
-    # Lee el archivo CSV y lo guarda en un DataFrame
-    data = pd.read_csv(archivo)
-    
-    # Muestra el DataFrame en la aplicación
-    st.write(data)
-
-  # Valida si el archivo cargado tiene extensión .xlsx
-  elif archivo.name.endswith(".xlsx"):
-
-    # Lee el archivo Excel y lo guarda en un DataFrame
-    data = pd.read_excel(archivo)
-    
-    # Muestra el DataFrame en la aplicación
-    st.write(data)
-
-  # Si el archivo no es CSV ni Excel, mostramos un mensaje de error
-  else:
-    st.write("Formato no válido")
-
-# Si el usuario no ha cargado ningún archivo, mostramos un mensaje
-else :
-  st.write("Por favor cargue su archivo")
-
+###########################################################################################################################################
